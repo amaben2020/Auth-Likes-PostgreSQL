6a2470a0df46bcad23dad8b9685bd33eab41d642
amaben2020 <amaben202phantom@gmail.com>
2021-03-05 23:29:20 +0100

    New updates made

---
+++ Index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const authRoute = require("./routes/auth");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const postRoute = require("./PrivateRoute/Posts");
+//Initializing express
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//puts/hides password when hosted
+dotenv.config();
+
+//Connecting with our db
+mongoose.connect(
+	process.env.DB_CONNECT,
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+	() => console.log("connected to database")
+);
+
+//Middlewares with authRoute as parameter
+app.use(express.json());
+app.use("/api/user", authRoute);
+app.use("/api/posts", postRoute);
+
+app.get("/", (req, res) => {
+	res.send("Use an ultra secure backend authentication service");
+});
+
+app.listen(process.env.PORT || 7000, () => {
+	console.log(`server is running excellently well `);
+});
